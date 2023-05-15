094fd3a1e194ee9b24190679bc650de1457fcdce
zhang <906556745@qq.com>
2023-05-15 17:02:34 +0800

    安装需要的模块

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
   </div>
 </template>
 
